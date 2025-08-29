cc272350b1943a6a19f2a8610e2bd5553936b35b
snwmarroquino <sokalrzz44@gmail.com>
2025-08-29 09:22:41 -0300

    Add files via upload

--- atividade1/main.js
+++ atividade1/main.js
@@ -1,11 +1,22 @@
 
 
-// cria referência ao form e ao elemento h3 (onde será exibida a resposta)
+// cria referência aos elementos da página
 const frm = document.querySelector("form")
-const resp = document.querySelector("h3")
+const resp1 = document.querySelector("#outMedicamento")
+const resp2 = document.querySelector("#outPromocao")
 
+// cria um "ouvinte" de evento, acionado quando o botão submit for clicado
 frm.addEventListener("submit", (e) => {
-    const nome = frm.inNome.value // obtém o nome digitado no form
-    resp.innerText = `olá, ${nome}` // exibe a resposta do programa
-    e.preventDefault () // evita envio do form
-} )
+    // obtém conteúdo dos campos de entrada
+    const medicamento = frm.inMedicamento.value
+    const preco = Number(frm.inPreco.value)
+
+    // calcula valor da promoção (arredonda para baixo)
+    const promocao = Math.floor(preco * 2)
+
+    // exibe as respostas
+    resp1.innerText = `Promoção de ${medicamento}`
+    resp2.innerText = `Leve 2 por apenas R$ ${promocao.toFixed(2)}`
+
+    e.preventDefault() // evita envio do form
+})
